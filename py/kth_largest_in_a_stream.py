08fb393dbca465f9b64fbd4781c96ff221bc5f7b
Srivastava, Yash <yash33srivastava@gmail.com>
2024-12-25 18:50:30 +0530

    another solution using heapq

--- py/kth_largest_in_a_stream.py
+++ py/kth_largest_in_a_stream.py
@@ -63,3 +63,25 @@ class KthLargest:
             self.pop()
         
         return self.heap[-self.k]
+
+
+#Another solution, using heapq
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.nums = nums
+        heapq.heapify(self.nums)
+        self.k = k
+        
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        
+        for _ in range(len(self.nums)-self.k):
+            heapq.heappop(self.nums)
+        
+        return self.nums[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
